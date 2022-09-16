c611f4f26e6ebd8becf012aea83a0536f12e75ca
Jacob Ogden <Jacob.ogden@yahoo.com>
2022-09-16 16:49:34 -0400

    Added Exercise 2.0 assignment.

---
+++ Exercise2.0/Exercise2.0.py
@@ -0,0 +1,24 @@
+print("Name: Jacob Ogden")
+print("Date: 9/16/2022")
+print("At what size of a project should you start using version management software?")
+print(
+    f"Version management software should be used for any project that is intended to be used "
+    f"more than once, if you are writing a single use script, you probably don't need version "
+    f"management software, but if you are creating a project meant to be used either by others "
+    f"or yourself more than once, you should use version management.\n"
+    f"Additionally you should use version management software if you are making a collaborative "
+    f"project. If there is more than one person working with and modifying the code of a project, "
+    f"it should almost certainly be using version management software."
+)
+print("Why?")
+print(
+    f"If at the very minimum, simply to make your life easier, there will be plenty of times "
+    f"where you commit a breaking change, or end up introducing a bug into your code that wasn't"
+    f"previously there, and version management will help you weed out the source of it.\n"
+    f"Additionally version management also conveniently acts as a backup of your project, "
+    f"if anything were to happen to your local project, you'll always have some sort of backup "
+    f"elsewhere, including backups of previous versions.\n"
+    f"During collaborative projects, version management software will also help significantly with "
+    f"keeping the project organized, and will make coordinating code changes with your collaborators "
+    f"much more seamless."
+)
