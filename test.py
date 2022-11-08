0c93a6f2380549731e84ef8dc26128d4ebe6cb94
Jacob Ogden <Jacob.ogden@yahoo.com>
2022-11-08 09:51:06 -0500

    a

--- test.py
+++
@@ -1,8 +0,0 @@
-import zlib
-
-import brotli
-
-with open("ascii-art.ans", "rb") as text:
-    content = brotli.decompress(text.read()).decode()
-    with open("ascii-art.ans", "wb") as text:
-        text.write(zlib.compress(content.encode()))
