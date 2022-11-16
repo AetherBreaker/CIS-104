bd0f1be09f3a1f47c2dd432bf841229b16f52522
Jacob Ogden <Jacob.ogden@yahoo.com>
2022-11-15 20:49:51 -0500

    Fix

--- Module8/exercise2.py
+++ Module8/exercise2.py
@@ -1,11 +1,11 @@
-
-
 import os
-from Module7.exercise2.2 import recurse_dir_search
 
+from Module7.exercise2 import recurse_dir_search
 
 if __name__ == "__main__":
-    fpath = recurse_dir_search("Exercise 2.py", os.path.dirname(os.path.abspath(__file__)))
+    fpath = recurse_dir_search(
+        "Exercise 2.py", os.path.dirname(os.path.abspath(__file__))
+    )
     fhand = open("mbox-short.txt")
     count = 0
     for line in fhand:
